fd9c9d84a4c61f47955e26510daa52482b6dab27
Michał Lower <keton22@gmail.com>
2020-03-23 17:00:11 +0100

    code cleanup

--- etrv2mqtt/cli.py
+++ etrv2mqtt/cli.py
@@ -12,17 +12,21 @@ from etrv2mqtt.mqtt import Mqtt
 import time
 from typing import Dict
 
+mqtt:Mqtt
+
+def poll_device(devices: Dict[str, eTRVDevice], name:str):
+    try:
+        device=devices[name]
+        logger.debug("Polling data from {}", name)
+        ret = eTRVUtils.read_device(device)
+        logger.debug(str(ret))
+        mqtt.publish_device_data(name, str(ret))
+    except btle.BTLEDisconnectError as e:
+        logger.error(e)
 
 def poll_devices(devices: Dict[str, eTRVDevice]):
-    for name, device in devices.items():
-        try:
-            logger.debug("Polling data from {}", name)
-            ret = eTRVUtils.read_device(device)
-            logger.debug(str(ret))
-            mqtt.publish_device_data(name, str(ret))
-        except btle.BTLEDisconnectError as e:
-            logger.error(e)
-
+    for name in devices.keys():
+        poll_device(devices, name)
 
 def set_temperature_callback(devices: Dict[str, eTRVDevice], name: str, temperature: float):
     if name not in devices.keys():
@@ -33,12 +37,13 @@ def set_temperature_callback(devices: Dict[str, eTRVDevice], name: str, temperat
         logger.debug("Setting {} to {}C", name, temperature)
         device = devices[name]
         eTRVUtils.set_temperature(device, temperature)
+        # Home assistant needs to see updated temperature value to confirm change
+        poll_device(devices, name)
     except btle.BTLEDisconnectError as e:
         logger.error(e)
 
 
 def main(config_file: str):
-    global config
 
     try:
         config = Config(config_file)
@@ -46,14 +51,13 @@ def main(config_file: str):
         logger.error(e)
         sys.exit(1)
 
-    global devices
-    devices = {}
+    devices:Dict[str, eTRVDevice] = {}
 
     for thermostat_config in config.thermostats.values():
         logger.debug("Adding {} MAC: {} key: {}", thermostat_config.topic, thermostat_config.address, thermostat_config.secret_key)
         device = eTRVUtils.create_device(thermostat_config.address, bytes.fromhex(thermostat_config.secret_key), retry_limit=config.retry_limit)
         devices[thermostat_config.topic] = device
-
+    
     global mqtt
     mqtt = Mqtt(config, devices, set_temperature_callback)
 
@@ -61,6 +65,13 @@ def main(config_file: str):
         poll_devices(devices)
         time.sleep(config.poll_interval)
 
+def usage():
+    print('Usage: '+sys.argv[0]+' configfile.json')
+    sys.exit(0)
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    if len(sys.argv) <2:
+        usage()
+    else:
+        main(sys.argv[1])
+    
--- etrv2mqtt/etrvutils.py
+++ etrv2mqtt/etrvutils.py
@@ -1,5 +1,17 @@
 from libetrv.device import eTRVDevice
 from libetrv.bluetooth import btle
+from dataclasses import dataclass,asdict
+
+
+@dataclass(repr=False)
+class eTRVData:
+    name: str
+    battery: int
+    room_temp: float
+    set_point: float
+
+    def __repr__(self):
+        return str(asdict(self))
 
 
 class eTRVUtils:
@@ -8,8 +20,8 @@ class eTRVUtils:
         return eTRVDevice(address, secret=key, retry_limit=retry_limit)
 
     @staticmethod
-    def read_device(device: eTRVDevice) -> dict:
-        return {'name': device.name, 'battery': device.battery, 'room_temp': device.temperature.room_temperature, 'set_point': device.temperature.set_point_temperature}
+    def read_device(device: eTRVDevice) -> eTRVData:
+        return eTRVData(device.name, device.battery, device.temperature.room_temperature, device.temperature.set_point_temperature)
 
     @staticmethod
     def set_temperature(device: eTRVDevice, temperature: float):
